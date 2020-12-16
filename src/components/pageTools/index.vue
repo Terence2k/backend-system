eeb1039e657dbfaf421a6492da7231d2e5de6b08
Terence <570120397@qq.com>
2020-12-16 20:44:34 +0800

    公共部分渲染样式

--- src/components/pageTools/index.vue
+++ src/components/pageTools/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="dashboard-container">
+  <div class="page-tools">
 
     <el-card>
       <el-row type="flex" justify="space-between" align="middle">
         <el-col>
-          <div v-if="left">
+          <div v-if="left" class="before">
 
             <i class="el-icon-info" />
             <slot name="before" />
@@ -23,10 +23,29 @@
 
 <script>
 export default {
-
+  props: {
+    left: {
+      type: Boolean,
+      default: false
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.page-tools {
+    margin: 10px 0;
+    .before {
+      line-height: 34px;
+    i {
+      margin-right: 5px;
+      color: #409eff;
+    }
+    display: inline-block;
+    padding: 0px 10px;
+    border-radius: 3px;
+    border: 1px solid rgba(145, 213, 255, 1);
+    background: rgba(230, 247, 255, 1);
+  }
+ }
 </style>
--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -4,13 +4,88 @@
       <h2>
         员工
       </h2>
+      <TopTools :left="true">
+        <span slot="before">共166条记录</span>
+        <template slot="after">
+          <el-button size="small" type="warning">导入</el-button>
+          <el-button size="small" type="danger">导出</el-button>
+          <el-button size="small" type="primary">新增员工</el-button>
+        </template>
+      </TopTools>
+      <el-card>
+
+        <el-table :data="list">
+          <el-table-column label="序号" prop="username">
+            <template slot-scope="{$index}">
+
+              {{ $index + 1 }}
+            </template>
+          </el-table-column>
+          <el-table-column label="用户名" prop="username" />
+          <el-table-column label="工号" prop="workNumber" />
+          <el-table-column label="聘用形式" prop="formOfEmployment" />
+          <el-table-column label="部门" prop="departmentName" />
+          <el-table-column label="入职时间" prop="timeOfEntry" />
+          <el-table-column label="庄户状态" prop="enableState" />
+          <el-table-column label="操作" prop="id" width="300px">
+            <template>
+              <el-button type="text" size="small">查看</el-button>
+              <el-button type="text" size="small">转正</el-button>
+              <el-button type="text" size="small">调岗</el-button>
+              <el-button type="text" size="small">离职</el-button>
+              <el-button type="text" size="small">角色</el-button>
+              <el-button type="text" size="small">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-row type="flex" justify="end" align="middle" style="height: 60px;">
+          <el-pagination
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="pageSetting.total"
+            :page-size="pageSetting.size"
+            :page-sizes="[2,8,10,20,50]"
+            @current-change="currentChange"
+            @size-change="sizeChange"
+          />
+        </el-row>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import { getUserList } from '@/api/employees'
+// import { getUserList } from '@/api/employess'
+// import { getUserList } from '@/api/employess'
 export default {
-
+  data() {
+    return {
+      list: [],
+      pageSetting: {
+        page: 1,
+        size: 8,
+        total: 20
+      }
+    }
+  },
+  created() {
+    this.getUserInfo()
+  },
+  methods: {
+    async  getUserInfo() {
+      const { rows, total } = await getUserList(this.pageSetting)
+      this.pageSetting.total = total
+      this.list = rows
+    },
+    currentChange(val) {
+      this.pageSetting.page = val
+      this.getUserInfo()
+    },
+    sizeChange(val) {
+      this.pageSetting.size = val
+      this.getUserInfo()
+    }
+  }
 }
 </script>
 
