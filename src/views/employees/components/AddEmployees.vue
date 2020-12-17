48bd36179ef3a5b932c9717c9998daea5d93699b
Terence <570120397@qq.com>
2020-12-17 14:48:54 +0800

    员工--新增模块渲染

---
+++ src/views/employees/components/AddEmployees.vue
@@ -0,0 +1,140 @@
+<template>
+
+  <el-dialog title="新增员工" :visible="isShow" @close="cancleBtn">
+
+    <el-form label-width="120px" :model="formData" :rules="rules">
+      <el-form-item label="姓名" prop="username">
+        <el-input
+          v-model="formData.username"
+          style="width: 50%"
+          placeholder="请输入姓名"
+        />
+      </el-form-item>
+      <el-form-item label="手机" prop="mobile">
+        <el-input
+          v-model="formData.mobile"
+          style="width: 50%"
+          placeholder="请输入手机号"
+        />
+      </el-form-item>
+      <el-form-item label="入职时间" prop="timeOfEntry">
+        <el-date-picker
+          v-model="formData.timeOfEntry"
+          style="width: 50%"
+          placeholder="请选择入职时间"
+        />
+      </el-form-item>
+      <el-form-item label="聘用形式" prop="formOfEmployment">
+        <el-select
+          v-model="formData.formOfEmployment"
+          style="width: 50%"
+          placeholder="请选择"
+        />
+      </el-form-item>
+      <el-form-item label="工号" prop="workNumber">
+        <el-input
+          v-model="formData.workNumber"
+          style="width: 50%"
+          placeholder="请输入工号"
+        />
+      </el-form-item>
+      <el-form-item label="部门" prop="departmentName">
+        <el-input
+          v-model="formData.departmentName"
+          style="width: 50%"
+          placeholder="请选择部门"
+          @focus="getDepartments"
+        />
+        <div v-if="treeData.length > 0" class="treeWrapper">
+          <el-tree
+            class="treeDeparment"
+            :data="treeData"
+            :props="{ label: 'name' }"
+            :default-expand-all="true"
+          />
+        </div>
+      </el-form-item>
+      <el-form-item label="转正时间" prop="correctionTime">
+        <el-date-picker
+          v-model="formData.correctionTime"
+          style="width: 50%"
+          placeholder="请选择转正时间"
+        />
+      </el-form-item>
+    </el-form>
+
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="cancleBtn">取 消</el-button>
+      <el-button type="primary" @click="sureInfo">确 定</el-button>
+    </div>
+  </el-dialog>
+
+</template>
+
+<script>
+export default {
+  props: {
+    isShow: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data() {
+    return {
+      treeData: [],
+      formData: {
+        username: '',
+        mobile: '',
+        formOfEmployment: '',
+        workNumber: '',
+        departmentName: '',
+        timeOfEntry: '',
+        correctionTime: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '用户姓名不能为空', trigger: 'blur' },
+          {
+            min: 1,
+            max: 4,
+            message: '用户姓名为1-4位'
+          }
+        ],
+        mobile: [
+          { required: true, message: '手机号不能为空', trigger: 'blur' },
+          {
+            pattern: /^1[3-9]\d{9}$/,
+            message: '手机号格式不正确',
+            trigger: 'blur'
+          }
+        ],
+        formOfEmployment: [
+          { required: true, message: '聘用形式不能为空', trigger: 'blur' }
+        ],
+        workNumber: [
+          { required: true, message: '工号不能为空', trigger: 'blur' }
+        ],
+        departmentName: [
+          { required: true, message: '部门不能为空', trigger: 'change' }
+        ],
+        timeOfEntry: [{ required: true, message: '入职时间', trigger: 'blur' }]
+      }
+    }
+  },
+  methods: {
+    sureInfo() {
+      console.log('ok')
+    },
+    getDepartments() {
+      console.log('555')
+    },
+    cancleBtn() {
+      this.$emit('update:isShow', false)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
