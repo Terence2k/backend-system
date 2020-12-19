02a444fb60f439815f99e83c38cb2c705133cae2
Terence <570120397@qq.com>
2020-12-19 10:17:06 +0800

    创建路由

--- src/router/modules/employees.js
+++ src/router/modules/employees.js
@@ -15,6 +15,16 @@ export default {
       title: '员工', // meta属性的里面的属性 随意定义 但是这里为什么要用title呢， 因为左侧导航会读取我们的路由里的meta里面的title作为显示菜单名称
       icon: 'people'
     }
+  },
+  {
+    name: 'dateil',
+    path: 'detail/:id',
+    hidden: true,
+    component: () => import('@/views/employees/detail'),
+    meta: {
+      title: '内部'
+
+    }
   }]
 }
 
