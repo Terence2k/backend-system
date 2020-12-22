99c072a4ee18cfccfd71c7ae722e6baefdf1eff5
Terence <570120397@qq.com>
2020-12-22 11:46:47 +0800

    有重复报错的permission的页面

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,16 +1,60 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        权限管理
-      </h2>
+      <TopTools>
+        <template slot="after">
+          <el-button>新增权限</el-button>
+        </template>
+      </TopTools>
+      <el-card>
+        <el-table :data="permissionList" row-key="id">
+          <el-table-column label="权限名字" prop="name" />
+          <el-table-column label="权限标识" prop="code" />
+          <el-table-column label="权限描述" prop="description" />
+          <el-table-column label="权限操作">
+            <template>
+              <el-button>权限</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import { getPermissionList } from '@/api/permission'
 export default {
+  data() {
+    return {
+      permissionList: []
+    }
+  },
+  created() {
+    this.getPermissionList()
+  },
+  methods: {
+    async getPermissionList() {
+      this.permissionList = await getPermissionList()
+      console.log(this.permissionList)
+      this.dealData(this.permissionList, '0')
+      console.log(this.permissionList)
+    },
+    dealData(data, init) {
+      const arr = []
+      data.forEach(item => {
+        if (item.pid === init) {
+          const children = this.dealData(data, item.id)
+          if (children.length > 0) {
+            item.children = children
+          }
+          arr.push(item)
+        }
+      })
+      return arr
+    }
 
+  }
 }
 </script>
 
